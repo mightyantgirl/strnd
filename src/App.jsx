f2539102ac0de351eaa18eda7cdb769a9c7b4206
mightyantgirl <155346084+mightyantgirl@users.noreply.github.com>
2026-06-07 02:56:46 +0900

    라우팅 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ export default function App() {
     <BrowserRouter>
       <div className="bg-bg px-5" style={{ height: '100dvh' }}>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </div>
     </BrowserRouter>
