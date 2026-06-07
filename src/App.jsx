e1524d5a7566cc75bb573e4633d2d43c9f1c7e66
mightyantgirl <155346084+mightyantgirl@users.noreply.github.com>
2026-06-07 14:52:05 +0900

    자동 로그인 분기 처리

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,10 @@ import Login from './pages/designer/login'
 import Register from './pages/designer/register'
 import Home from './pages/designer/home'
 
-const isLoggedIn = () => {
-  return localStorage.getItem('token') || sessionStorage.getItem('token')
-}
-
 export default function App() {
+  const isLoggedIn = () => {
+    return localStorage.getItem('token') || sessionStorage.getItem('token')
+  }
   return (
     <BrowserRouter>
       <div className="bg-bg px-5" style={{ height: '100dvh' }}>
