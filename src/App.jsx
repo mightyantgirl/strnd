fe4975dd8ff2b146f0a84f0ab0097baf227a7cef
mightyantgirl <155346084+mightyantgirl@users.noreply.github.com>
2026-06-07 14:08:38 +0900

    자동 로그인 분기 처리

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 // App.jsx
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/designer/login'
 import Register from './pages/designer/register'
 import Home from './pages/designer/home'
 
+const isLoggedIn = () => {
+  return localStorage.getItem('token') || sessionStorage.getItem('token')
+}
+
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="bg-bg px-5" style={{ height: '100dvh' }}>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </div>
+      <Routes>
+        {/* 루트 접속 시 토큰 있으면 홈, 없으면 로그인 */}
+        <Route path="/" element={isLoggedIn() ? <Navigate to="/home" /> : <Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </BrowserRouter>
   )
 }
