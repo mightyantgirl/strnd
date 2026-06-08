94a1c74491494fd7b2420e5b21f9a7076ddadff7
mightyantgirl <155346084+mightyantgirl@users.noreply.github.com>
2026-06-09 01:21:51 +0900

    홈/신규고객등록/고객상세 화면 구현 및 API 연동

--- src/components/pageheader.jsx
+++ src/components/pageheader.jsx
@@ -1,8 +1,8 @@
 export default function PageHeader({ onBack, title }) {
   return (
-    <div className="w-full bg-bg fixed pr-10 pl-1 pb-6 z-10">
+    <div className="w-full bg-bg fixed pr-10 pl-1 pb-3 z-10">
       <div className="flex gap-5 pr-5 mt-6 items-center">
-        <button onClick={onBack}>
+        <button onClick={onBack} className="px-3 py-3">
           <img src="/img/back.svg" alt="뒤로가기" />
         </button>
 
