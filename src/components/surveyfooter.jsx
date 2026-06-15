c583b931dc6171172f1e16661c68615ddad6e3e4
mightyantgirl <155346084+mightyantgirl@users.noreply.github.com>
2026-06-15 19:36:55 +0900

    fix:푸터 cta버튼 padding값 조정

--- src/components/surveyfooter.jsx
+++ src/components/surveyfooter.jsx
@@ -4,7 +4,7 @@ const baseTextClass = `text-xs text-placeholder font-medium`
 
 export default function SurveyFooter({ onNext, disabled, children, value }) {
   return (
-    <div className="shrink-0 bg-bg pt-4 flex flex-col items-center">
+    <div className="shrink-0 bg-bg pt-4 pb-4flex flex-col items-center">
       {children && <span className={`${baseTextClass} mb-4`}>{children}</span>}
       <Button variant="primary" value={value} height="md" onClick={onNext} disabled={disabled} />
     </div>
